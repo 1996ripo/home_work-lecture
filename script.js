188d037974348c89bf58ec69e02ab9497df8f9ec
Hripsime <1996ripo@gmail.com>
2022-01-25 07:49:15 -0800

    other

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 let input = document.getElementById('input');
 let submit = document.getElementById('submit');
 
+let actionDocument = ['scroll', 'click', 'keypress'];
+let actionInput = ['focus', 'blur', 'input'];
 
-window.addEventListener('scroll',e=>{
-    console.log('scroll');
+// document.addEventListener('scroll',e=>{
+//     console.log('scroll');
+// });
+
+// document.addEventListener('click', e => {
+//     console.log('click' + ' x=' + e.x + ' y=' + e.y);
+// });
+
+// document.addEventListener('keypress', e => {
+//     console.log('keypress ' + e.key);
+// });
+
+// input.addEventListener('focus', e => {
+//     console.log('focus ' + ' input');
+// });
+
+// input.addEventListener('blur', e => {
+//     console.log('blur ' + ' input');
+// });
+
+// input.addEventListener('input', e => {
+//     console.log('input ' + e.data);
+// });
+
+actionDocument.forEach(element => {
+    document.addEventListener(element, e => {
+       console.log(element);
+    });
 });
-
-document.addEventListener('click', e => {
-    console.log('click' + ' x=' + e.x + ' y=' + e.y);
-});
-
-document.addEventListener('keypress', e => {
-    console.log('keypress ' + e.key);
-});
-
-input.addEventListener('focus', e => {
-    console.log('focus ' + ' input');
-});
-
-input.addEventListener('blur', e => {
-    console.log('focus ' + ' input');
-});
-
-input.addEventListener('input', e => {
-    console.log('input ' + e.data);
-});
-
