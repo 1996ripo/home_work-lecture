c50647435abd89098d2fb4c8b9a3c4e4c17b08de
Hripsime <1996ripo@gmail.com>
2022-01-25 08:09:29 -0800

    home work new

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
 let input = document.getElementById('input');
 let submit = document.getElementById('submit');
+let  inf=document.getElementById('inf');
 
 let actionDocument = ['scroll', 'click', 'keypress'];
 let actionInput = ['focus', 'blur', 'input'];
 
-// document.addEventListener('scroll',e=>{
-//     console.log('scroll');
-// });
-
-// document.addEventListener('click', e => {
-//     console.log('click' + ' x=' + e.x + ' y=' + e.y);
-// });
-
-// document.addEventListener('keypress', e => {
-//     console.log('keypress ' + e.key);
-// });
-
-// input.addEventListener('focus', e => {
-//     console.log('focus ' + ' input');
-// });
-
-// input.addEventListener('blur', e => {
-//     console.log('blur ' + ' input');
-// });
-
-// input.addEventListener('input', e => {
-//     console.log('input ' + e.data);
-// });
 
 actionDocument.forEach(element => {
     document.addEventListener(element, e => {
-       console.log(element);
+        inf.insertAdjacentHTML('beforeend',`<li>${element}</li>`);
     });
 });
+
+actionInput.forEach(element => {
+    document.addEventListener(element, e => {
+        inf.insertAdjacentHTML('beforeend', `<li>${element}.......value is ${e.target.value} </li>`);
+    });
+});
+
